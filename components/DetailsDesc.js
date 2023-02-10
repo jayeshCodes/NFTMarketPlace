a7bdd3471e203db5615b59326900e330b217d15d
Jayesh <1032190683@mitwpu.edu.in>
2023-02-10 15:27:10 +0530

    Full commit

---
+++ components/DetailsDesc.js
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DetailsDesc = () => {
+  return (
+    <View>
+      <Text>DetailsDesc</Text>
+    </View>
+  )
+}
+
+export default DetailsDesc
+
+// https://youtu.be/_ivIUCSOZ78?t=6206
